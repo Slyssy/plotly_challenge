3d1f13ddc4506cc52f7fb0f6752aec97ef6bb4a7
Stephen <stephen.lyssy@gmail.com>
2020-11-28 13:36:31 -0600

    Haven't gotten much accomplished. Just monkeying around with the dropdown menu. I can't get it to work.

--- belly_button_biodiversity/README.md
+++ belly_button_biodiversity/README.md
@@ -8,7 +8,7 @@ The dataset reveals that a small handful of microbial species (also called opera
 
 ## Step 1: Plotly
 
-1. Use the D3 library to read in `samples.json`.
+[x] 1. Use the D3 library to read in `samples.json`.
 
 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 
--- belly_button_biodiversity/app.js
+++ belly_button_biodiversity/app.js
@@ -1,6 +1,24 @@
 d3.json("samples.json").then(
     data => {
         console.log("Hello")
-        console.log(data)       
+        // console.log(data)
+        console.log(data.samples.map(d => d.id))
+        // console.log(data.names.map(d => d.names))
+        // console.log(data.samples.map(d => d.sample_values))
+        // console.log(data.samples.map(d => d.otu_ids))     
+        // let trace = [{
+        //     type: 'bar',
+        //     x: data.samples.map(d => d.sample_values),
+        //     y: data.samples.map(d => d.otu_ids),
+        //     orientation: 'h'
+        // }];
+        // Plotly.newPlot("bar", data)
 }
     )
+
+// let var data = [{
+//     type: 'bar',
+//     x: data.samples.map(d => d.sample_values),
+//     y: data.samples.map(d => d.otu_ids),
+//     orientation: 'h'
+// }]
