ce665806f8e01ecded5a1bda7acee8d602991051
Stephen <stephen.lyssy@gmail.com>
2020-11-29 07:41:34 -0600

    Updated app to define the input for the annonymous function so that it can be used outside of the function. I used this to pull the data from the sample json to create the drop down menu data.Finally, I set the created a function to log the clicks from the dropdown menu and passed that varialbe to through the find method to show only the data associated with the idselected in the drop down menu.

--- belly_button_biodiversity/app.js
+++ belly_button_biodiversity/app.js
@@ -1,25 +1,43 @@
+var dataset;
+
 d3.json("samples.json").then(
-    data => {   
-            
+    data => {  
+        var names = data.names;
+        var ele = document.getElementById("selDataset");
+        for(var i = 0; i < names.length; i++) {
+          var opt = names[i];
+
+          var el = document.createElement("option");
+          el.text = opt;
+          el.value = opt;
+
+          ele.add(el);
+        } 
+        dataset = data   
         // console.log("Hello")
-        console.log(data)
+        
         // console.log(data.samples.map(d => d.id))
         // console.log(data.names.map(d => d.names))
         // console.log(data.samples.map(d => d.sample_values))
         // console.log(data.samples.map(d => d.otu_ids))     
-        // let trace = [{
-        //     type: 'bar',
-        //     x: data.samples.map(d => d.sample_values),
-        //     y: data.samples.map(d => d.otu_ids),
-        //     orientation: 'h'
-        // }];
-        // Plotly.newPlot("bar", data)
+        
 }
     )
 
-// let var data = [{
-//     type: 'bar',
-//     x: data.samples.map(d => d.sample_values),
-//     y: data.samples.map(d => d.otu_ids),
-//     orientation: 'h'
-// }]
+    function optionChanged(value) {
+            console.log('clicked:', value);
+            console.log(dataset);
+
+        var sample = dataset.samples.find(d => d.id === value)
+            console.log(sample)
+
+        let trace = [{
+            type: 'bar',
+            x: sample.sample_values,
+            y: sample.otu_ids,
+            orientation: 'h'
+        }];
+        Plotly.newPlot("bar", trace)
+        }   
+        
+        
